4b9d3c38cc500f3b66c53ce450f5c2833dd25fe7
Speykious <speykious@gmail.com>
2021-08-11 19:16:41 +0200

    I just learned about the alias region. The second peak is actually to be expected!

--- dft.plot.c
+++ dft.plot.c
@@ -7,14 +7,12 @@
 int main() {
   size_t size = 8192;
   float* sine_440 = generate_sinewave(44100, 10000, 1, size);
-  float* sine_cpy = copy_floats(sine_440, size);
+  float* sine_cpy = calloc(size, sizeof(float));//copy_floats(sine_440, size);
   
   dft(sine_440, sine_cpy, size);
 
   for (size_t i = 0; i < size; i++)
-    printf("%ld %20f\n", i,
-      sqrt(sine_440[i] * sine_440[i] + sine_cpy[i] * sine_cpy[i])
-    );
+    printf("%ld %20f\n", i, sine_440[i]);
 
   free(sine_440);
   free(sine_cpy);
